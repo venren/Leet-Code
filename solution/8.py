7a8a4df318d9b4af3148ce18ec77accc14081e37
venren <v.renganathanit@gmail.com>
2024-01-21 18:12:41 +0000

    Add leetcode questions for 8 and 9.

--- solution/8.py
+++ solution/8.py
@@ -1,6 +1,9 @@
-#https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/566/
 from typing import List
 
+#Given an integer array nums, find the subarray with the largest sum, and return its sum.
+#Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+#Output: 6
+#Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
--- solution/9.py
+++ solution/9.py
@@ -1,5 +1,9 @@
 from typing import List
 
+# Input: citations = [3,0,6,1,5]
+# Output: 3
+# Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+# Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
 
 class Solution:
     def hIndexOrderOfNSolution(self, citations: List[int]) -> int:
