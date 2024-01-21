a500a8adeed8c9b8eeb25f95e5a1d8fb57256edc
venren <v.renganathanit@gmail.com>
2024-01-21 18:14:39 +0000

    Add leetcode questions for 8 and 9.

--- solution/8.py
+++ solution/8.py
@@ -1,9 +1,10 @@
 from typing import List
 
-#Given an integer array nums, find the subarray with the largest sum, and return its sum.
-#Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-#Output: 6
-#Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+# Given an integer array nums, find the subarray with the largest sum, and return its sum.
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
@@ -35,8 +36,7 @@ class Solution:
 
 
 Sol = Solution()
-print(Sol.maxSubArray([-2,-1])) #-1
-print(Sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) #6
-print(Sol.maxSubArray([5,4,-1,7,8])) #23
-print(Sol.maxSubArray([1])) #1
-
+print(Sol.maxSubArray([-2, -1]))  # -1
+print(Sol.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))  # 6
+print(Sol.maxSubArray([5, 4, -1, 7, 8]))  # 23
+print(Sol.maxSubArray([1]))  # 1
--- solution/9.py
+++ solution/9.py
@@ -1,11 +1,11 @@
 from typing import List
 
-# Input: citations = [3,0,6,1,5]
-# Output: 3
-# Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
-# Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
 
-#According to the definition of h-index on Wikipedia:
+# Input: citations = [3,0,6,1,5] Output: 3 Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and
+# each of them had received 3, 0, 6, 1, 5 citations respectively. Since the researcher has 3 papers with at least 3
+# citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+# According to the definition of h-index on Wikipedia:
 # The h-index is defined as the maximum value of h such that the given researcher has published at least h
 # papers that have each been cited at least h times.
 
@@ -19,7 +19,7 @@ class Solution:
         if len(citations) == 1 and citations[0] > 0:
             return 1
 
-        for i in range(0,len(citations),1):
+        for i in range(0, len(citations), 1):
             if citations[i] != 0:
                 currentCitation = min(len(citations) - i, citations[i])
 
@@ -37,7 +37,7 @@ class Solution:
         if len(citations) == 1 and citations[0] > 0:
             return 1
 
-        for i in range(len(citations)-1,-1,-1):
+        for i in range(len(citations) - 1, -1, -1):
 
             if citations[i] < maxCitation:
                 return maxCitation
@@ -57,4 +57,4 @@ print(Sol.hIndexOrderOfLogNSolution([1, 100, 1]))  # 1
 print(Sol.hIndexOrderOfLogNSolution([10]))  # 1
 print(Sol.hIndexOrderOfLogNSolution([0]))  # 0
 print(Sol.hIndexOrderOfLogNSolution([11, 15]))  # 2
-print(Sol.hIndexOrderOfLogNSolution([0,1,0]))  # 1
+print(Sol.hIndexOrderOfLogNSolution([0, 1, 0]))  # 1
